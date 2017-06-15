69d44ab78d705cd8b272ebd8c603d14345b9d329
hanpenghu <563909408@qq.com>
2017-06-15 10:41:49 +0800

    2017_6_15   weekday(4)   10:41:45

---
+++ src/com/ManJiaSonPort/sync/dao/InsertCategoryDao.java
@@ -0,0 +1,11 @@
+package com.ManJiaSonPort.sync.dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/6/15.
+ */
+@Component
+public class InsertCategoryDao {
+
+}
--- src/com/ManJiaSonPort/sync/test/test_schedule/ScheduleTest.java
+++ src/com/ManJiaSonPort/sync/test/test_schedule/ScheduleTest.java
@@ -3,14 +3,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
-
 /**
  * Created by Administrator on 2017/6/15.
  */
@@ -58,3 +55,24 @@ public class ScheduleTest {
 /////////////////////////////////2017_6_15   weekday(4)   9:45:14////////////////////////////////////////
 }
 ////////////////////////////////////2017_6_15   weekday(4)   9:45:08/////////////////////////////////////
+
+
+/*
+0 0 12 * * ?	每天12点触发
+        0 15 10 ? * *	每天10点15分触发
+        0 15 10 * * ?	每天10点15分触发
+        0 15 10 * * ? *	每天10点15分触发
+        0 15 10 * * ? 2005	2005年每天10点15分触发
+        0 * 14 * * ?	每天下午的 2点到2点59分每分触发
+        0 0/5 14 * * ?	每天下午的 2点到2点59分(整点开始，每隔5分触发)
+        0 0/5 14,18 * * ?	每天下午的 2点到2点59分(整点开始，每隔5分触发) 每天下午的 18点到18点59分(整点开始，每隔5分触发)
+        0 0-5 14 * * ?	每天下午的 2点到2点05分每分触发
+        0 10,44 14 ? 3 WED	3月分每周三下午的 2点10分和2点44分触发
+        0 15 10 ? * MON-FRI	从周一到周五每天上午的10点15分触发
+        0 15 10 15 * ?	每月15号上午10点15分触发
+        0 15 10 L * ?	每月最后一天的10点15分触发
+        0 15 10 ? * 6L	每月最后一周的星期五的10点15分触发
+        0 15 10 ? * 6L 2002-2005	从2002年到2005年每月最后一周的星期五的10点15分触发
+        0 15 10 ? * 6#3	每月的第三周的星期五开始触发
+        0 0 12 1/5 * ?	每月的第一个中午开始每隔5天触发一次
+        0 11 11 11 11 ?	每年的11月11号 11点11分触发(光棍节)*/
